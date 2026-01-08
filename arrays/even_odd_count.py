9e20831614fd7eb88e0a655d218d0c08a89f6cca
adityaraj-dev1 <aadiraj426@gmail.com>
2026-01-08 20:18:29 +0530

    Add program to count even and odd numbers in array

---
+++ arrays/even_odd_count.py
@@ -0,0 +1,13 @@
+arr = [1, 2, 3, 4, 5, 6]
+
+even = 0
+odd = 0
+
+for x in arr:
+    if x % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Even numbers count:", even)
+print("Odd numbers count:", odd)
