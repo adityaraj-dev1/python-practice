8690c6e747afa4eef2695ceea99f2d428b0bf35f
adityaraj-dev1 <aadiraj426@gmail.com>
2026-01-21 18:15:13 +0530

    Improve number guessing game with loop

--- input_programs/number_guess.py
+++ input_programs/number_guess.py
@@ -2,9 +2,11 @@ import random
 
 secret = random.randint(1, 10)
 
-guess = int(input("Guess a number between 1 and 10: "))
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
 
-if guess == secret:
-    print("Correct! 🎉")
-else:
-    print("Wrong! Number was:", secret)
+    if guess == secret:
+        print("Correct! 🎉")
+        break
+    else:
+        print("Wrong! Try again.")
